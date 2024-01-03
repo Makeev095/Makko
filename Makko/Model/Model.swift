f6517d1d276741f74743bfb8fa552382b6b18267
Макей 😈 <imac_makey@iMac-Makej.local>
2024-01-03 18:50:36 +0300

    Slider is almost ready

---
+++ Makko/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Makko
+//
+//  Created by Макей 😈 on 03.01.2024.
+//
+
+import Foundation
+import UIKit
+
+struct Slides {
+    var id: Int
+    var text: String
+    var img: UIImage
+}
---
+++ Makko/View/Slide/SlideCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  SlideCollectionViewCell.swift
+//  Makko
+//
+//  Created by Макей 😈 on 03.01.2024.
+//
+
+import UIKit
+
+class SlideCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var descriptionText: UILabel!
+    @IBOutlet weak var slideImage: UIImageView!
+    
+    @IBOutlet weak var regButton: UIButton!
+    @IBOutlet weak var authButton: UIButton!
+    
+    static var reuseId = "SlideCollectionViewCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configure(slide: Slides) {
+        slideImage.image = slide.img
+        descriptionText.text = slide.text
+        
+        if slide.id == 3 {
+            regButton.isHidden = false
+            authButton.isHidden = false
+        }
+    }
+    
+    @IBAction func regButtonClick(_ sender: Any) {
+        
+    }
+    
+    @IBAction func authButtonClick(_ sender: Any) {
+        
+    }
+}
