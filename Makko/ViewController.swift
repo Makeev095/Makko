529649753631f775a4203ec697b86e4355875f53
Макей 😈 <imac_makey@iMac-Makej.local>
2024-01-03 16:59:47 +0300

    First settings for repository

---
+++ Makko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Makko
+//
+//  Created by Макей 😈 on 03.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
